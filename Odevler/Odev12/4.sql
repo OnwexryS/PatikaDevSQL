33cf73bc1ac7be9097b824d2baa07db5f78c8cec
furkanaliunal <unconfigured@null.spigotmc.org>
2021-07-25 14:46:33 +0300

    Homework-12/4 Updated

--- Odevler/Odev12/4.sql
+++ Odevler/Odev12/4.sql
@@ -1,13 +1,24 @@
 -- payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
 
-SELECT (
-	SELECT COUNT(*)
-	FROM payment
-	WHERE payment.customer_id = customer.customer_id
-) AS "Count of purchases", *
-FROM customer
-ORDER BY (
-	SELECT COUNT(*) 
-	FROM payment 
-	WHERE payment.customer_id = customer.customer_id
-) DESC;
+
+SELECT COUNT(*) AS "Sipariş Sayısı", customer.first_name, customer.last_name, customer.customer_id FROM customer
+JOIN payment ON customer.customer_id = payment.customer_id
+
+GROUP BY customer.first_name, customer.last_name, customer.customer_id
+ORDER BY COUNT(*) DESC;
+
+
+
+
+
+--SELECT (
+--	SELECT COUNT(*)
+--	FROM payment
+--	WHERE payment.customer_id = customer.customer_id
+--) AS "Count of purchases", *
+--FROM customer
+--ORDER BY (
+--	SELECT COUNT(*) 
+--	FROM payment 
+--	WHERE payment.customer_id = customer.customer_id
+--) DESC;
